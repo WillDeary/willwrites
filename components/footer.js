ec9f610530eec8481aab5e5c11c9e69a7696e5b7
Will Deary <will@willdeary.co.uk>
2021-05-06 16:43:31 +0200

    Update footer to include social media icons

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,4 @@
+import { SocialIcon } from 'react-social-icons';
 export default function Footer() {
     return (
         <footer className="text-gray-600 body-font">
@@ -6,17 +7,17 @@ export default function Footer() {
                     <span className="ml-3 text-xl">Will Writes</span>
                 </a>
                 <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-
+                    All content written by me, myself and I
                 </p>
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                   <a className="text-gray-500">
+                    <SocialIcon url="https://github.com/WillDeary" />
                   </a>
                   <a className="ml-3 text-gray-500">
+                      <SocialIcon url="https://www.instagram.com/willdeary" />
                   </a>
                   <a className="ml-3 text-gray-500">
-                  </a>
-                  <a className="ml-3 text-gray-500">
-
+                      <SocialIcon url="https://www.linkedin.com/in/william-deary-017319151/" />
                   </a>
                 </span>
             </div>
