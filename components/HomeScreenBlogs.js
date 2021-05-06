343f08a6d4486e826cdec007e9b206a9d42681d5
Will Deary <will@willdeary.co.uk>
2021-05-06 16:24:36 +0200

    Disgusting workaround for tailwind dynamic class purging

--- components/HomeScreenBlogs.js
+++ components/HomeScreenBlogs.js
@@ -13,7 +13,16 @@ export function HomeScreenBlogs({allPostData}) {
                             <div
                                 className="px-12 pt-4 flex flex-col items-start border-b-2 border-gray-100 cursor-pointer">
                                     <span
-                                        className={`inline-block py-1 px-2 rounded bg-${color}-50 text-${color}-500 text-xs font-medium tracking-widest`}>
+                                        {/*Disgusting way to do this, however tailwind purges dynamic classnames
+                                        so this is the work around. If we add more colors add then in here.... Puke */}
+                                        className={`inline-block py-1 px-2 rounded                                        
+                                        ${color === 'yellow' ? 'bg-yellow-50 text-yellow-500 ' 
+                                            : color === 'blue' ? 'bg-blue-50 text-blue-500 ' 
+                                                : color === "red" ? 'bg-red-50 text-red-500 ' 
+                                                    : color === "green" ? 'bg-green-50 text-green-500 '
+                                                        : ''
+                                        }                                      
+                                        text-xs font-medium tracking-widest`}>
                                         {category}
                                     </span>
                                 <h2 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-1 mb-4">
